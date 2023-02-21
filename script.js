a6e623466434f5e16c4454cfbbfcf51ede972d9c
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-21 02:02:17 +0100

    ajout du js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-let btn = document.querySelector('.btn')
+let btn = document.querySelector(".btn");
 
 function flip(){
     let coin = Math.round(Math.random());
     let img = document.querySelector("img");
+    coin === 1 ? (img.src = "heads.png") : (img.src = "tails.png");
 }
 
-btn.addEventListener('click', flip);
+btn.addEventListener("click", flip);
