4d5b4151b69c533c14de9ee6661e540dc1761b64
SabrinaMouginot <s.mouginot@yahoo.fr>
2023-02-21 01:27:48 +0100

    creation du btn en js plutot qu en html

---
+++ script.js
@@ -0,0 +1,8 @@
+let btn = document.querySelector('.btn')
+
+function flip(){
+    let coin = Math.round(Math.random());
+    let img = document.querySelector("img");
+}
+
+btn.addEventListener('click', flip);
